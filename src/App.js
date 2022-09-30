ad359e932c061d28bd99805f34860897a26f4c98
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-09-30 17:46:53 -0300

    added theme provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './routes/public/Home';
-import New from "./routes/public/New"
-
+import { createTheme, ThemeProvider , colors} from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/public/Home";
+import New from "./routes/public/New";
+const theme = createTheme({
+  palette: { 
+    primary: {
+      main: colors.grey[50]
+    },
+    secondary: {
+      main: colors.blue[500]
+    }
+  }
+})
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/news' element={<New />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<New />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
