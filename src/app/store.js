8e78a69c0f36272c4aaaf068ed8b3b4705337f02
Marco Romero <marcod.romero@gmail.com>
2022-09-30 07:24:51 -0300

    Remove everything related to redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import alertReducer from '../features/alert/alertSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    alertIsVisible: alertReducer,
   },
 });
--- src/features/alert/Alert.js
+++ src/features/alert/Alert.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import SweetAlert from 'sweetalert2-react';
-import {
-  hide,
-  selectAlert,
-} from './alertSlice';
-import styles from './Alert.module.css';
 
-export function Alert({type, title, text}){
-    const alertIsVisible = useSelector(selectAlert);
-    const dispatch = useDispatch();
-    
+export function Alert({type, title, text, show}){
     return(
         <SweetAlert
-            show= {alertIsVisible}
             type = {type}
             title= {title}
             text= {text}
-            onConfirm={() => dispatch(hide())}
+            show = {show}
          />
     );
 }
+
