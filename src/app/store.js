7218e7b8320b6658387318e4b498b877215d813c
Marco Romero <marcod.romero@gmail.com>
2022-09-30 09:44:22 -0300

    remove everything related to redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import loaderReducer from '../features/loader/loaderSlice';
-
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    isLoading: loaderReducer
   },
 });
--- src/features/loader/Loader.js
+++ src/features/loader/Loader.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import styles from './Loader.module.css';
 import { RotatingLines } from 'react-loader-spinner';
-import { useSelector} from 'react-redux';
-import { selectLoader } from './loaderSlice';
 
-export function Loader({strokeColor, strokeWidth, width}){
-    const isLoading = useSelector(selectLoader);
-    
+export function Loader({visible, strokeColor, strokeWidth, width}){   
     return(
         <div>
             <RotatingLines
-                visible= {isLoading}
-                strokeColor= {strokeColor || "grey"}
-                strokeWidth= {strokeWidth || "5"}
-                width= {width || "30"}            
+                visible= {visible}
+                strokeColor= {strokeColor}
+                strokeWidth= {strokeWidth}
+                width= {width}            
             />
         </div>
     );
