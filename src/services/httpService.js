6df88b549f5656f89bf751b0b923232e0d96868b
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-04 18:54:07 -0500

    Created http service

---
+++ src/services/httpService.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+axios.interceptors.request.use(
+    function (config) {
+
+        config.headers.Authorization = `Bearer ${sessionStorage.getItem('token')}`
+        config.baseURL = `http://localhost:5000`
+        return config
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+)
+
+export default {
+    get: axios.get,
+    post: axios.post
+}
