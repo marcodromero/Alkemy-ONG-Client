2158c6452cbf4c5afe313383334d998332fc7c47
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-10 18:33:39 -0300

    implemented login form

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,97 @@
+import { Box, TextField, Button, Typography, Link } from "@mui/material";
+import {useFormik } from "formik";
+import React, { useEffect } from "react";
+import * as yup from "yup";
+import httpService from "../../services/httpService";
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string("Enter your password")
+    .min(6, "Password should be of minimum 6 characters length")
+    .required("Password is required"),
+});
+
+export default function LoginForm() {
+    useEffect(() => {
+        if(sessionStorage.getItem('token')){
+            window.location = '/'
+        }
+    })
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, actions) => {
+      try {
+        const req = await httpService.post("/auth/login", values);
+        sessionStorage.setItem("token", req.data.Authorization);
+      } catch (e) {
+        console.log(e.response.data);
+        if (e.response.data === "Email not found") {
+          actions.setErrors({ email: "Email not found" });
+        } else {
+          actions.setErrors({ password: "Password is incorrect" });
+        }
+      }
+    },
+  });
+  return (
+    <Box
+      sx={{
+        maxWidth: "600px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        margin: "0 auto",
+      }}
+    >
+      <form onSubmit={formik.handleSubmit}>
+        <Typography
+          sx={{ marginBottom: "1.5rem" }}
+          align="center"
+          variant="h4"
+          component="h1"
+          gutterBottom
+        >
+          {" "}
+          Login{" "}
+        </Typography>
+        <TextField
+          sx={{ marginBottom: "1.5rem" }}
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          sx={{ marginBottom: "1.5rem" }}
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="secondary" variant="contained" fullWidth type="submit">
+          Login
+        </Button>
+        <Typography>
+          Don't have a Account yet?<Link href="/register"><Typography color="secondary" sx={{display: 'inline'}}>Sign up!</Typography></Link>
+        </Typography>
+      </form>
+    </Box>
+  );
+}
