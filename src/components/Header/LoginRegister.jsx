7ef4f84a2ec15c4742eb23ae0f8a8a7815e7cff4
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-10 18:32:21 -0300

    added links for login and register routes

--- src/components/Header/LoginRegister.jsx
+++ src/components/Header/LoginRegister.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
-import {
-  Typography,
-  Menu,
-  IconButton,
-  MenuItem,
-} from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
+import { Box, Button, Link } from "@mui/material";
+import { Typography, Menu, IconButton, MenuItem } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
 const pages = [
-  "Login",
-  "Registrate",
+  {
+    text: "Login",
+    route: "/login",
+  },
+  {
+    text: "Register",
+    route: "/register",
+  },
 ];
+const linkNoDecoration = {
+  textDecoration: "none",
+};
 export default function LoginRegister() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const handleOpenNavMenu = (event) => {
@@ -24,48 +28,64 @@ export default function LoginRegister() {
     <>
       <Box sx={{ display: { xs: "none", md: "flex" } }}>
         <Button sx={{ mr: 1 }} variant="contained">
-          Login
+          <Link sx={{ textDecoration: "none", color: "black" }} href="/login">
+            Login
+          </Link>
         </Button>
         <Button variant="contained" color="success">
-          Registrate
+          <Link sx={linkNoDecoration} href="/register">
+            Register
+          </Link>
         </Button>
       </Box>
-      <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, flexDirection: "row-reverse"}}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <PersonIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}  sx={{textTransform: "capitalize" }}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: "flex", md: "none" },
+          flexDirection: "row-reverse",
+        }}
+      >
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <PersonIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: "block", md: "none" },
+          }}
+        >
+          {pages.map((page) => (
+            <Link sx={linkNoDecoration} href={page.route}>
+              <MenuItem
+                key={page.text}
+                onClick={handleCloseNavMenu}
+                sx={{ textTransform: "capitalize" }}
+              >
+                <Typography color={'black'} textAlign="center">{page.text}</Typography>
+              </MenuItem>
+            </Link>
+          ))}
+        </Menu>
+      </Box>
     </>
   );
 }
