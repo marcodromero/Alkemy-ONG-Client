8099acf448e48f45f9a7d9a13fce65f84b362dee
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-20 01:21:58 -0300

    fix profilebutton links

--- src/components/Header/ProfileButton.jsx
+++ src/components/Header/ProfileButton.jsx
@@ -6,7 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
-import { Link } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 const adminSettings = [
 
   {
@@ -16,16 +17,10 @@ const adminSettings = [
 ]
 const userSettings = [
   {
-    text: 'Profile',
-    route: '/profile',
-  },
-  {
     text: 'Logout',
     route: '/logout'
   }
 ]
-// const userSettings = ["Profile", "Logout"];
-
 export default function ProfileButton({admin, user}) {
     const handleOpenUserMenu = (event) => {
         setAnchorElUser(event.currentTarget);
@@ -38,11 +33,16 @@ export default function ProfileButton({admin, user}) {
     <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src={user.image} />
+                <Avatar alt="Remy Sharp" src={user?.image } />
               </IconButton>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: '45px' ,
+              '& a': {
+                textDecoration: 'none',
+                color: 'black'
+              }
+            }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
@@ -58,14 +58,14 @@ export default function ProfileButton({admin, user}) {
               onClose={handleCloseUserMenu}
             >
               {admin && adminSettings.map((setting, i) => (
-                  <Link key={i} href={setting.route} sx={{color: 'black', textDecoration: 'none'}}>
+                  <Link key={i} to={setting.route}>
                 <MenuItem  onClick={handleCloseUserMenu}>
                     <Typography textAlign="center">{setting.text}</Typography>
                 </MenuItem>
                   </Link>
               ))}
               {userSettings.map((setting, i) => (
-                  <Link key={i} href={setting.route} sx={{color: 'black', textDecoration: 'none'}}>
+                  <Link key={i} to={setting.route}>
                 <MenuItem  onClick={handleCloseUserMenu}>
                     <Typography textAlign="center">{setting.text}</Typography>
                 </MenuItem>
