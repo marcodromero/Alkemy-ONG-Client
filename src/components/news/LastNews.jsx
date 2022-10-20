5b33bf00e0e8beaa1f760e424e31e211d56f578e
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-20 16:46:09 -0300

    LastNews is now dynamic

--- src/components/news/LastNews.jsx
+++ src/components/news/LastNews.jsx
@@ -1,30 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import NewCard from "./NewCard";
 import { useNavigate } from "react-router-dom";
 
-const news = [
-  {
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat repellendus. Magni necessitatibus harum modi inventore deserunt similique, repellat earum.",
-    image: "images/popular-01.jpg",
-  },
-  {
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat repellendus. Magni necessitatibus harum modi inventore deserunt similique, repellat earum.",
-    image: "images/popular-02.jpg",
-  },
-  {
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat repellendus. Magni necessitatibus harum modi inventore deserunt similique, repellat earum.",
-    image: "images/popular-03.jpg",
-  },
-  {
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, fugiat repellendus. Magni necessitatibus harum modi inventore deserunt similique, repellat earum.",
-    image: "images/journal-01.jpg",
-  },
-];
+import httpService from '../../services/httpService'
 
 const LastNews = () => {
   const navigate = useNavigate()
+  
+  const [news, setNews] = React.useState([]);
+  useEffect(() => {
+    (async () => {
+     const res = await httpService.get('/news')
+      setNews(res.data)
+    })();
+  }, [])
   return (
     <>
       <Typography
@@ -55,15 +46,18 @@ const LastNews = () => {
           margin: 0,
           position: "initial",
           display: "flex",
-          overflowX: "hidden",
+          flexWrap: "wrap",
+          justifyContent: "center",
           paddingLeft: "10%",
           paddingRight: "10%",
           gap: 10,
         }}
       >
-        {news.slice(0, 2).map(({ text, image }) => {
-          return <NewCard text={text} image={image} />;
+
+        {news.slice(0, 2).map(({ content, image, name, id }) => {
+          return <NewCard key={id} content={content} name={name} image={image} id={id} />;
         })}
+        
       </Box>
     </>
   );
