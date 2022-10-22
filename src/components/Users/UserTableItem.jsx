4ea1a21dcf86174504d1bfcc02c7f50fe7a3cda7
FelipeLL <f3lip3.3z@hotmail.com>
2022-10-22 12:55:10 -0500

    Design review Users

--- src/components/Users/UserTableItem.jsx
+++ src/components/Users/UserTableItem.jsx
@@ -1,49 +1,58 @@
-import { Delete, Edit } from "@mui/icons-material";
+import { Delete, Edit, Create } from "@mui/icons-material";
 
-import { Button, colors, TableCell, TableRow} from "@mui/material";
+import {
+  Button,
+  colors,
+  TableCell,
+  TableRow,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
 
 import React from "react";
 import httpService from "../../services/httpService";
 import { DeleteAlert } from "../../features/alert/Alert";
 import { Link, useNavigate } from "react-router-dom";
 export default function UserTableItem({ firstName, lastName, email, id }) {
-  const navigate = useNavigate()
-  const [showDeleteWarning, setShowDeleteWarning] = React.useState(false)
+  const navigate = useNavigate();
+  const [showDeleteWarning, setShowDeleteWarning] = React.useState(false);
   const initDelete = () => {
-    setShowDeleteWarning(true)
-  }
+    setShowDeleteWarning(true);
+  };
   return (
     <>
-    <DeleteAlert
-    show={showDeleteWarning}
-    onConfirm={() => {
-      httpService.delete(`/users/${id}`)
-      setShowDeleteWarning(false)
-      navigate('/backoffice/users')
-    }}
-    onCancel={() => setShowDeleteWarning(false)}
-    text='¿Esta seguro que desea eliminar este usuario?'
-    />
-    
-    <TableRow>
-      <TableCell>{firstName}</TableCell>
-      <TableCell>{lastName}</TableCell>
-      <TableCell>{email}</TableCell>
-      <TableCell sx={{
-        '& a': {
-          textDecoration: 'none',
-          color: colors.lightBlue[500],
-        }
-      }}>
+      <DeleteAlert
+        show={showDeleteWarning}
+        onConfirm={() => {
+          httpService.delete(`/users/${id}`);
+          setShowDeleteWarning(false);
+          navigate("/backoffice/users");
+        }}
+        onCancel={() => setShowDeleteWarning(false)}
+        text="¿Esta seguro que desea eliminar este usuario?"
+      />
 
-        <Link to={`/backoffice/users/${id}`}>
-          <Edit />
-        </Link>
-        <Link onClick={() => initDelete()} color='secondary'>
-          <Delete />
-        </Link>
-      </TableCell>
-    </TableRow>
+      <TableRow>
+        <TableCell size="small">{firstName}</TableCell>
+        <TableCell size="small">{lastName}</TableCell>
+        <TableCell size="small">{email}</TableCell>
+        <TableCell size="small">
+          <Tooltip title="Eliminar">
+            <Link onClick={() => initDelete()} color="secondary">
+              <IconButton>
+                <Delete color="danger" />
+              </IconButton>
+            </Link>
+          </Tooltip>
+          <Tooltip title="Actualizar">
+            <Link to={`/backoffice/users/${id}`}>
+              <IconButton>
+                <Create />
+              </IconButton>
+            </Link>
+          </Tooltip>
+        </TableCell>
+      </TableRow>
     </>
   );
 }
