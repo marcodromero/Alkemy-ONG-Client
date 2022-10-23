27afaa125cb61466edd9fb95a2bd525b902e23c8
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-10-23 16:37:42 -0300

    redesign cover component

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,109 +1,75 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import { Button, Grid, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
 import { useNavigate } from "react-router-dom";
-import { width } from "@mui/system";
 
 export default function Cover() {
   const navigate = useNavigate();
   return (
-    <Card
+    <Paper
+    elevation={8}
       sx={{
-        display: "flex",
-        flexDirection: { xs: "column", md: "row" },
+        width: "90%",
+        m: '0 auto',
         boxSizing: "border-box",
-        border: "2px solid #FFFFFF",
-        outline: "none",
-        width: "100%",
-        height: "100%",
-        paddingLeft: "9%",
-        paddingRight: "9%",
+        p: 4,
+        borderRadius: 4
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "50%",
-        }}
-      >
-        <CardContent
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            margin: "auto",
-          }}
-        >
-          <Typography
-            component="div"
-            variant="h2"
+      <Grid container spacing={2}>
+        
+        <Grid item md={12} lg={6} order={{md: 2, lg: 1}}>
+          <Box
             sx={{
-              color: "#000000",
-              fontSize: "46px",
-              fontWeight: 600,
-              width: { xs: "200%", md: "100%" },
-              textAlign: { xs: "center", md: "left" },
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: 'space-around'
             }}
           >
-            Hola! Bienvenidx
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            component="div"
+            <Typography  component="h2" variant="h2" mt={5} mb={5}>
+              <b>Hola, ¡Bienvenidx!</b>
+            </Typography>
+            <Typography component="p" variant="h6" mt={5} mb={5}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              repudiandae recusandae enim, atque consequatur placeat fugit
+              voluptatem totam rem quidem nam laboriosam quas assumenda neque sunt
+              quasi aliquid quisquam impedit!
+            </Typography>
+            <Box>
+              <Button
+                variant="contained"
+                color="secondary"
+                sx={{
+                  color: '#fff'
+                }}
+                onClick={() => navigate("/activities")}
+              >
+                Contactanos
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item md={12} lg={6} order={{md: 2, lg: 1}}>
+          <Box
             sx={{
-              marginTop: 2,
-              color: "#000000",
-              fontSize: "24px",
-              fontWeight: "ligth",
-              lineHeight: 1.3,
-              width: { xs: "200%", md: "100%" },
-              textAlign: { xs: "justify", md: "left" },
+              "& img": {
+                width: "100%",
+                height: "100%",
+                aspectRatio: "3/2",
+                objectFit: "cover",
+                borderRadius: 4,
+              },
             }}
           >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing
-            dignissim ac et eleifend lacus, rhoncus, dignissim sagittis. Tellus
-            ac a, elementum ut. Tellus a morbi tincidunt ultricies malesuada
-            eget turpis. Lacus enim non enim, velit hac turpis interdum arcu.
-            Suspendisse at vel ultrices amet orci enim lectus porttitor ut.
-          </Typography>
-          <Button
-            variant="contained"
-            color="danger"
-            sx={{
-              backgroundColor: "#FF0000",
-              color: "#fff",
-              width: "40%",
-              marginTop: 5,
-              textTransform: "capitalize",
-              fontSize: 24,
-              borderRadius: 2,
-            }}
-            onClick={() => navigate("/contact")}
-          >
-            Contactanos
-          </Button>
-        </CardContent>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{
-          width: { xs: "100%", md: "47%" },
-          height: "70vh",
-          borderRadius: 5,
-          marginTop: "auto",
-          marginBottom: "auto",
-          marginLeft: "auto",
-        }}
-        image={process.env.PUBLIC_URL + "images/blog-img-03.jpg"}
-        alt="cover image"
-      />
-    </Card>
+            <img
+              src={process.env.PUBLIC_URL + "/images/blog-img-02.jpg"}
+              alt="cover image"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
